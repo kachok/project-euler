fc727a6a3355ff46ab1b0af7c6701ff7af3d2855
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-05-04 13:51:02 -0400

    Implemented calculation for problem 15 in js

--- js/015.js
+++ js/015.js
@@ -1,2 +1,9 @@
 //40!/(20!*20!)
 //http://en.wikipedia.org/wiki/Permutations#Permutations_of_multisets
+
+function factorial(n) {
+	if (n===1) return 1;
+	else return n*factorial(n-1);
+}
+
+console.log(factorial(40)/(factorial(20)*factorial(20)));
